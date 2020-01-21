2cf4afd2493d7026fce2c76ca73f466990d6c1a9
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-21 16:27:10 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,18 +1,9 @@
 class Author 
   attr_accessor :name
   
-  @@all = []
-  
   def initialize(name)
     @name = name 
     @@all << self 
   end 
-  
-  def self.all
-    @@all 
-  end 
-  
-  def add_post 
-    post.author = self 
-  end 
+
 end 
