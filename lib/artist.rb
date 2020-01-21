d8450b243321916131347052767e85c543d7675f
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-21 15:44:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,6 +24,6 @@ class Artist
   def add_song_by_name(song_name)
     #name_of_song = Song.new(song_name)
     name_of_song = Song.new(song_name)
-    add_song(song_name)
+    add_song(name_of_song)
   end 
 end 
